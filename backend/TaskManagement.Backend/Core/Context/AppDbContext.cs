3e85fcbac64c78b1550774b6a541757d61fd00eb
Paw565pl <paw565pl@gmail.com>
2025-05-08 20:49:44 +0200

    chore: add new entities to db context

--- backend/TaskManagement.Backend/Core/Context/AppDbContext.cs
+++ backend/TaskManagement.Backend/Core/Context/AppDbContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using TaskManagement.Backend.Features.Project.Entity;
+using TaskManagement.Backend.Features.Task.Entity;
 
 namespace TaskManagement.Backend.Core.Context;
 
 public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : DbContext(dbContextOptions)
 {
-
+    public DbSet<ProjectEntity> ProjectEntities => Set<ProjectEntity>();
+    public DbSet<TaskEntity> TaskEntities => Set<TaskEntity>();
 }
