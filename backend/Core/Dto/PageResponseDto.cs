6ad69b9a5470e4901761878ac7ce8122035a7685
Paw565pl <paw565pl@gmail.com>
2025-05-09 21:33:26 +0200

    feat: create page response dto

---
+++ backend/Core/Dto/PageResponseDto.cs
@@ -0,0 +1,13 @@
+namespace TaskManagement.Backend.Core.Dto;
+
+public record PageResponseDto<T>(
+    IList<T> Content,
+    int TotalItems,
+    int PageNumber,
+    int PageSize
+)
+{
+    public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1 && PageNumber <= TotalPages;
+}
