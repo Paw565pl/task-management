0d477d755447895332c729b3189f6ec4e0249be3
Paw565pl <paw565pl@gmail.com>
2025-05-09 15:04:32 +0200

    feat: use explicit annotations

--- backend/Features/Project/Entity/ProjectEntity.cs
+++ backend/Features/Project/Entity/ProjectEntity.cs
@@ -1,4 +1,4 @@
-using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using TaskManagement.Backend.Features.Task.Entity;
@@ -8,19 +8,19 @@ namespace TaskManagement.Backend.Features.Project.Entity;
 [Table("projects"), Index(nameof(Name), IsUnique = true), Index(nameof(CreatedAt)), Index(nameof(UpdatedAt))]
 public class ProjectEntity
 {
-    public long Id { get; set; }
+    [Key, Column("id")] public long Id { get; set; }
 
-    [Column(TypeName = "varchar(200)")]
-    public required string Name { get; set; }
+    [Column("name"), MaxLength(200)] public required string Name { get; set; }
 
-    [Column(TypeName = "varchar(2000)")]
+    [Column("description"), MaxLength(2000)]
     public string? Description { get; set; }
 
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public DateTime? CreatedAt { get; set; }
+    [Column("created_at"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; }
 
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    public DateTime? UpdatedAt { get; set; }
+    [Column("updated_at"), DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedAt { get; set; }
 
-    public Collection<TaskEntity> Tasks { get; } = [];
+    [InverseProperty(nameof(TaskEntity.Project))]
+    public IList<TaskEntity> Tasks { get; } = [];
 }
--- backend/Features/Task/Entity/TaskEntity.cs
+++ backend/Features/Task/Entity/TaskEntity.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using TaskManagement.Backend.Features.Project.Entity;
@@ -7,26 +8,28 @@ namespace TaskManagement.Backend.Features.Task.Entity;
 [Table("tasks"), Index(nameof(CreatedAt)), Index(nameof(UpdatedAt))]
 public class TaskEntity
 {
-    public long Id { get; set; }
+    [Key, Column("id")] public long Id { get; set; }
 
-    [Column(TypeName = "varchar(200)")]
-    public required string Title { get; set; }
+    [Column("title"), MaxLength(200)] public required string Title { get; set; }
 
-    [Column(TypeName = "varchar(2000)")]
+    [Column("description"), MaxLength(2000)]
     public string? Description { get; set; }
 
-    public TaskStatus TaskStatus { get; set; } = TaskStatus.Todo;
+    [Column("task_status")] public TaskStatus TaskStatus { get; set; } = TaskStatus.Todo;
 
-    public required TaskPriority TaskPriority { get; set; }
+    [Column("task_priority")] public required TaskPriority TaskPriority { get; set; }
 
-    public required DateOnly DueDate { get; set; }
+    [Column("due_date")] public required DateOnly DueDate { get; set; }
 
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public DateTime? CreatedAt { get; set; }
+    [Column("created_at"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; }
 
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    public DateTime? UpdatedAt { get; set; }
+    [Column("updated_at"), DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedAt { get; set; }
 
+    [Column("project_id"), ForeignKey(nameof(Project))]
     public long ProjectId { get; set; }
+
+    [InverseProperty(nameof(ProjectEntity.Tasks)), DeleteBehavior(DeleteBehavior.Cascade)]
     public required ProjectEntity Project { get; set; }
 }
