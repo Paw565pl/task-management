3dd7cf0726860b3819aa79291cfce7cc15c39fc8
Paw565pl <paw565pl@gmail.com>
2025-05-09 20:52:29 +0200

    feat: add validation attributes

--- backend/Features/Project/Dto/ProjectRequestDto.cs
+++ backend/Features/Project/Dto/ProjectRequestDto.cs
@@ -1,7 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using TaskManagement.Backend.Core.Validators;
 
 namespace TaskManagement.Backend.Features.Project.Dto;
 
 public record ProjectRequestDto(
-    [Required] string Name,
-    string? Description);
+    [NotBlank, StringLength(200, MinimumLength = 5)] string Name,
+    [StringLength(2000, MinimumLength = 10)] string? Description);
