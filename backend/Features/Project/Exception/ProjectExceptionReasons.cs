da08b21de199ded12ab628dcb7dbcdf66d318227
Paw565pl <paw565pl@gmail.com>
2025-05-10 14:08:13 +0200

    feat: create project exception reasons

---
+++ backend/Features/Project/Exception/ProjectExceptionReasons.cs
@@ -0,0 +1,9 @@
+using TaskManagement.Backend.Core.ExceptionHandler;
+
+namespace TaskManagement.Backend.Features.Project.Exception;
+
+public static class ProjectExceptionReasons
+{
+    public static ProblemDetailsExceptionReason NotFound =>
+        new(StatusCodes.Status404NotFound, "Project with given id does not exist.");
+}
