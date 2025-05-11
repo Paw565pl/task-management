4618b39bf06c5520bb495e748579df93a85d770a
Paw565pl <paw565pl@gmail.com>
2025-05-11 14:53:51 +0200

    feat: serialize enums as strings

--- backend/Features/Task/Entity/TaskStatus.cs
+++ backend/Features/Task/Entity/TaskStatus.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace TaskManagement.Backend.Features.Task.Entity;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum TaskStatus
 {
     Todo,
