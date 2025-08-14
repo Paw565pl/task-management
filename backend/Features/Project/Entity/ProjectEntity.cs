584311dfb37f621784a3f5297cf389abdd6c04c7
Paw565pl <paw565pl@gmail.com>
2025-08-14 18:16:48 +0200

    feat: clean up attributes, set timestamps in the db on entity create

--- backend/Features/Project/Entity/ProjectEntity.cs
+++ backend/Features/Project/Entity/ProjectEntity.cs
@@ -1,24 +1,41 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TaskManagement.Backend.Features.Task.Entity;
 
 namespace TaskManagement.Backend.Features.Project.Entity;
 
-[Table("projects"), Index(nameof(Name), IsUnique = true), Index(nameof(CreatedAt)), Index(nameof(UpdatedAt))]
+[Table("projects")]
+[Index(nameof(Name), IsUnique = true)]
+[Index(nameof(CreatedAt))]
+[Index(nameof(UpdatedAt))]
+[EntityTypeConfiguration(typeof(ProjectEntityConfiguration))]
 public class ProjectEntity
 {
-    [Key, Column("id")] public long Id { get; set; }
+    [Key] [Column("id")] public long Id { get; set; }
 
-    [Column("name"), MaxLength(200)] public required string Name { get; set; }
+    [MaxLength(250)] [Column("name")] public required string Name { get; set; }
 
-    [Column("description"), MaxLength(2000)]
+    [MaxLength(5000)]
+    [Column("description")]
     public string? Description { get; set; }
 
-    [Column("created_at")] public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    [Column("created_at")] public DateTime CreatedAt { get; private init; }
 
-    [Column("updated_at")] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    [Column("updated_at")] public DateTime UpdatedAt { get; private set; }
 
     [InverseProperty(nameof(TaskEntity.Project))]
-    public IList<TaskEntity> Tasks { get; } = [];
+    public ICollection<TaskEntity> Tasks { get; } = [];
+
+    public void RefreshUpdatedAt() => UpdatedAt = DateTime.UtcNow;
+}
+
+internal class ProjectEntityConfiguration : IEntityTypeConfiguration<ProjectEntity>
+{
+    public void Configure(EntityTypeBuilder<ProjectEntity> builder)
+    {
+        builder.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP").ValueGeneratedOnAdd();
+        builder.Property(x => x.UpdatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP").ValueGeneratedOnAdd();
+    }
 }
--- backend/Features/Task/Entity/TaskEntity.cs
+++ backend/Features/Task/Entity/TaskEntity.cs
@@ -1,18 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TaskManagement.Backend.Features.Project.Entity;
 
 namespace TaskManagement.Backend.Features.Task.Entity;
 
-[Table("tasks"), Index(nameof(Status)), Index(nameof(Priority)), Index(nameof(DueDate)), Index(nameof(CreatedAt)), Index(nameof(UpdatedAt))]
+[Table("tasks")]
+[Index(nameof(Status))]
+[Index(nameof(Priority))]
+[Index(nameof(DueDate))]
+[Index(nameof(CreatedAt))]
+[Index(nameof(UpdatedAt))]
+[EntityTypeConfiguration(typeof(TaskEntityConfiguration))]
 public class TaskEntity
 {
-    [Key, Column("id")] public long Id { get; set; }
+    [Key] [Column("id")] public long Id { get; set; }
 
-    [Column("title"), MaxLength(200)] public required string Title { get; set; }
+    [MaxLength(250)] [Column("title")] public required string Title { get; set; }
 
-    [Column("description"), MaxLength(2000)]
+    [MaxLength(5000)]
+    [Column("description")]
     public string? Description { get; set; }
 
     [Column("status")] public TaskStatus Status { get; set; } = TaskStatus.Todo;
@@ -21,13 +29,28 @@ public class TaskEntity
 
     [Column("due_date")] public required DateOnly DueDate { get; set; }
 
-    [Column("created_at")] public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    [Column("created_at")] public DateTime CreatedAt { get; private init; }
 
-    [Column("updated_at")] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    [Column("updated_at")] public DateTime UpdatedAt { get; private set; }
 
-    [Column("project_id"), ForeignKey(nameof(Project))]
-    public long ProjectId { get; set; }
+    [ForeignKey(nameof(Project))]
+    [Column("project_id")]
+    public long ProjectId { get; init; }
 
-    [InverseProperty(nameof(ProjectEntity.Tasks)), DeleteBehavior(DeleteBehavior.Cascade)]
-    public required ProjectEntity Project { get; set; }
+    [InverseProperty(nameof(ProjectEntity.Tasks))]
+    [DeleteBehavior(DeleteBehavior.Cascade)]
+    public required ProjectEntity? Project { get; init; }
+
+    public void RefreshUpdatedAt() => UpdatedAt = DateTime.UtcNow;
+}
+
+internal class TaskEntityConfiguration : IEntityTypeConfiguration<TaskEntity>
+{
+    public void Configure(EntityTypeBuilder<TaskEntity> builder)
+    {
+        builder.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP").ValueGeneratedOnAdd();
+        builder.Property(x => x.UpdatedAt)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP").ValueGeneratedOnAdd(
+            );
+    }
 }
