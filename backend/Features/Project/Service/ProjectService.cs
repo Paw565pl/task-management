a6aa399b84309ca5f4f78166c47c79dfd2761747
Paw565pl <paw565pl@gmail.com>
2025-08-14 19:28:38 +0200

    refactor: improve sorting logic

--- backend/Features/Project/Service/ProjectService.cs
+++ backend/Features/Project/Service/ProjectService.cs
@@ -1,8 +1,10 @@
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using TaskManagement.Backend.Core.Context;
 using TaskManagement.Backend.Core.Dto;
 using TaskManagement.Backend.Core.ExceptionHandler;
 using TaskManagement.Backend.Features.Project.Dto;
+using TaskManagement.Backend.Features.Project.Entity;
 using TaskManagement.Backend.Features.Project.Exception;
 using TaskManagement.Backend.Features.Project.Mapper;
 
@@ -10,32 +12,36 @@ namespace TaskManagement.Backend.Features.Project.Service;
 
 public class ProjectService(AppDbContext appDbContext)
 {
-    public async Task<PageResponseDto<ProjectResponseDto>> GetAllAsync(SortOptionsDto? sortOptionsDto,
-        PageOptionsDto? pageOptionsDto)
+    private static readonly Dictionary<string, Expression<Func<ProjectEntity, object>>> SortColumns =
+        new(StringComparer.OrdinalIgnoreCase)
+        {
+            ["name"] = x => x.Name,
+            ["createdat"] = x => x.CreatedAt,
+            ["updatedat"] = x => x.UpdatedAt,
+        };
+
+    public async Task<PageResponseDto<ProjectResponseDto>> GetAllAsync(SortOptionsDto? sortOptionsDto = null,
+        PageOptionsDto? pageOptionsDto = null)
     {
         var query = appDbContext.Projects.AsNoTracking();
-
-        var sortField = sortOptionsDto?.SortBy?.ToLower();
-        var sortDirection = sortOptionsDto?.SortDirection;
-        query = sortField switch
-        {
-            "name" => sortDirection == SortDirection.Asc
-                ? query.OrderBy(p => p.Name).ThenBy(p => p.Id)
-                : query.OrderByDescending(p => p.Name).ThenBy(p => p.Id),
-            "createdat" => sortDirection == SortDirection.Asc
-                ? query.OrderBy(p => p.CreatedAt).ThenBy(p => p.Id)
-                : query.OrderByDescending(p => p.CreatedAt).ThenBy(p => p.Id),
-            "updatedat" => sortDirection == SortDirection.Asc
-                ? query.OrderBy(p => p.UpdatedAt).ThenBy(p => p.Id)
-                : query.OrderByDescending(p => p.UpdatedAt).ThenBy(p => p.Id),
-            _ => query.OrderBy(p => p.Id)
-        };
 
         var pageNumber = pageOptionsDto?.PageNumber ?? 1;
         var pageSize = pageOptionsDto?.PageSize ?? 20;
 
         var total = await query.CountAsync();
         if (total == 0) return new([], total, pageNumber, pageSize);
+
+        if (SortColumns.TryGetValue(sortOptionsDto?.SortBy ?? string.Empty, out var keySelector))
+        {
+            query =
+                sortOptionsDto?.SortDirection == SortDirection.Asc
+                    ? query.OrderBy(keySelector).ThenBy(p => p.Id)
+                    : query.OrderByDescending(keySelector).ThenBy(p => p.Id);
+        }
+        else
+        {
+            query = query.OrderBy(p => p.Id);
+        }
 
         query = query.Skip((pageNumber - 1) * pageSize).Take(pageSize);
         var content = await query.ToResponseDto().ToListAsync();
